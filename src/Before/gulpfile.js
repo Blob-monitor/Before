0b49c0ec20b17bf0e9c8a16f5ace9a50ad97332f
brollins90 <brollins90@gmail.com>
2015-12-30 19:52:56 -0700

    add bower and some script libraries

--- src/Before/gulpfile.js
+++ src/Before/gulpfile.js
@@ -2,13 +2,15 @@
 "use strict";
 
 var gulp = require("gulp"),
-    rimraf = require("rimraf"),
     concat = require("gulp-concat"),
     cssmin = require("gulp-cssmin"),
-    uglify = require("gulp-uglify");
+    ts = require('gulp-typescript'),
+    uglify = require("gulp-uglify"),
+    rimraf = require("rimraf"),
+    project = require("./project.json");
 
 var paths = {
-    webroot: "./wwwroot/"
+  webroot: "./wwwroot/"
 };
 
 paths.js = paths.webroot + "js/**/*.js";
@@ -19,27 +21,31 @@ paths.concatJsDest = paths.webroot + "js/site.min.js";
 paths.concatCssDest = paths.webroot + "css/site.min.css";
 
 gulp.task("clean:js", function (cb) {
-    rimraf(paths.concatJsDest, cb);
+  rimraf(paths.concatJsDest, cb);
 });
 
 gulp.task("clean:css", function (cb) {
-    rimraf(paths.concatCssDest, cb);
+  rimraf(paths.concatCssDest, cb);
 });
 
 gulp.task("clean", ["clean:js", "clean:css"]);
 
 gulp.task("min:js", function () {
-    return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
-        .pipe(concat(paths.concatJsDest))
-        .pipe(uglify())
-        .pipe(gulp.dest("."));
+  return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
+      .pipe(concat(paths.concatJsDest))
+      .pipe(uglify())
+      .pipe(gulp.dest("."));
 });
 
 gulp.task("min:css", function () {
-    return gulp.src([paths.css, "!" + paths.minCss])
-        .pipe(concat(paths.concatCssDest))
-        .pipe(cssmin())
-        .pipe(gulp.dest("."));
+  return gulp.src([paths.css, "!" + paths.minCss])
+      .pipe(concat(paths.concatCssDest))
+      .pipe(cssmin())
+      .pipe(gulp.dest("."));
 });
 
 gulp.task("min", ["min:js", "min:css"]);
+
+gulp.task("watch", function () {
+  gulp.watch(paths.css, ["min"]);
+})
